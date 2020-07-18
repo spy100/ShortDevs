4b466ee5c8c3428fe8cbebfd92a28c4ad3acc88c
spy100 <wdesigner2010@gmail.com>
2020-07-18 23:13:21 +0300

    Add files via upload

---
+++ 404.php
@@ -0,0 +1,175 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href='https://fonts.googleapis.com/css?family=Roboto+Condensed' rel='stylesheet'>
+    <title>ShortDevs</title>
+    <style>
+
+html,body{
+  background:black;
+  margin:0;
+  padding:0;
+  text-align:center;
+}
+
+* {
+  box-sizing: border-box;
+}
+
+@font-face {
+  font-family: "Roboto Condensed";
+  src: url("fonts/roboto-condensedregular.eot"); 
+  src: url("fonts/roboto-condensedregular.eot?#iefix") format("embedded-opentype"), 
+    url("fonts/roboto-condensedregular.otf") format("opentype"), 
+    url("fonts/roboto-condensedregular.svg") format("svg"), 
+    url("fonts/roboto-condensedregular.ttf") format("truetype"), 
+    url("fonts/roboto-condensedregular.woff") format("woff"), 
+    url("fonts/roboto-condensedregular.woff2") format("woff2"); 
+  font-weight: normal;
+  font-style: normal;
+}
+
+
+label{
+  font-family: 'Roboto Condensed', sans-serif;
+  font-size:14px;
+}
+
+pre.error{
+  color:red;
+}
+
+
+input:focus, input.form-control:focus {
+outline:none !important;
+outline-width: 0 !important;
+box-shadow: none;
+-moz-box-shadow: none;
+-webkit-box-shadow: none;
+}
+
+.actinstall{
+  background:#f4f4f4;
+  border:1px solid #ccc;
+  padding:10px;
+  margin-top:10px;
+  margin-left:auto;
+  margin-right:auto;
+  width:36%;
+}
+
+.actinstall2{
+  background:#f4f4f4;
+  border:1px solid #ccc;
+  padding:10px;
+  margin-top:30px;
+  margin-left:auto;
+  margin-right:auto;
+  width:36%;
+}
+
+.actinstall input[type="text"]{
+  width:100%;
+  text-indent: 10px;
+  padding:10px 0px;
+  margin-top:-1px;
+
+}
+
+.actinstall input[type="email"]{
+  width:100%;
+  text-indent: 10px;
+  padding:10px 0px;
+}
+
+.actinstall input[type="password"]{
+  width:100%;
+  text-indent: 10px;
+  padding:10px 0px;
+
+}
+@media screen and (max-width: 800px) {
+  .actinstall{
+    width:96%;
+  }
+
+  .actinstall2{
+    width:96%;
+  }
+}
+
+.actnote{
+  text-align:center;
+  padding-top:10px;
+}
+
+h2{
+  font-family: 'Roboto Condensed', sans-serif;
+  font-weight: normal;
+  padding:0;
+  margin:0;
+}
+
+
+.logonav{
+ color:#FFF;
+ overflow:hidden;
+ font-size:30px;
+ background:black;
+ padding:10px 10px 10px 10px;
+ margin-top:15%;
+}
+.logo{
+  font-family: 'Roboto Condensed', sans-serif;
+  font-size:60px;
+  float:left;
+  width:100%;
+  text-align:center;
+}
+
+.gold{
+  color: gold;
+}
+
+.smallv{
+  font-size:13px;
+}
+
+.error{
+  color:white;
+  font-family: 'Roboto Condensed', sans-serif;
+  font-size:25px;
+  text-transform:uppercase;
+}
+.four{
+  font-family: 'Roboto Condensed', sans-serif;
+  color:white;
+  font-size:80px;
+}
+
+.gohome{
+  color:white;
+  font-family: 'Roboto Condensed', sans-serif;
+  font-size:16px;
+}
+a{
+  color:gold;
+}
+
+   </style>
+  </head>
+  <body>
+
+
+<div class="logonav" ><div class="logo" >[ Short<span class="gold">Devs</span> <span class="smallv" >v.1.0.0</span> ]</div></div>
+<div class="four" >404</div>
+<div class="error" >Error - Page Not Found</div>
+<div class="gohome" ><a href="index.php" >Click here</a> to go back to Home Page</div>
+
+
+
+</body>
+</html>
